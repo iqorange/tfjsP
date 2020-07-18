1f7d882b332ee787ad0746dc0c0faa13ae7dbc6c
iqorange <39493393+iqorange@users.noreply.github.com>
2020-07-18 23:32:24 +0800

    RESTful

--- Koa/index.js
+++ Koa/index.js
@@ -1,7 +1,13 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const cors = require('@koa/cors')
+const koaBody = require('koa-body')
+const json = require('koa-json')
 const app = new Koa()
 const router = new Router()
+
+// 前缀
+router.prefix('/api')
 
 router.get('/', ctx => {
     console.log(ctx);
@@ -10,9 +16,17 @@ router.get('/', ctx => {
 })
 
 router.get('/api', ctx => {
+    // get params
+    const params = ctx.request.query
+    console.log(params);
+    console.log(params.name, params.age);
+
     console.log(ctx);
     console.log(ctx.request);
-    ctx.body = 'Hello Api~'
+    ctx.body = {
+        name: params.name,
+        age: params.age
+    }
 })
 
 router.get('/async', async(ctx) => {
@@ -24,6 +38,23 @@ router.get('/async', async(ctx) => {
     ctx.body = result;
 })
 
+router.post('/post', async(ctx) => {
+    // ES6把请求体中的内容全部放到body里去
+    let {body} = ctx.request;
+    console.log(body);
+    console.log(ctx.request);
+    ctx.body = {
+        // 扩展运算符，把body展开来返回回去，避免循环嵌套
+        ...body
+    }
+})
+
+// 首先处理request过来的数据
+app.use(koaBody())
+// 处理跨域请求问题
+app.use(cors())
+// json format (http://localhost:3000/api/api?name=kb&age=15&pretty)
+app.use(json({pretty: false, param: 'pretty'}))
 // 配置中间件
 app.use(router.routes()).use(router.allowedMethods())// 拦截器
 
