793f94924e72d89d8d98ab8d8398034fa786e346
iqorange <39493393+iqorange@users.noreply.github.com>
2020-07-18 22:57:28 +0800

    async&await

--- Koa/index.js
+++ Koa/index.js
@@ -15,6 +15,15 @@ router.get('/api', ctx => {
     ctx.body = 'Hello Api~'
 })
 
+router.get('/async', async(ctx) => {
+    let result = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Hello async 2s later~');
+        }, 2000);
+    })
+    ctx.body = result;
+})
+
 // 配置中间件
 app.use(router.routes()).use(router.allowedMethods())// 拦截器
 
