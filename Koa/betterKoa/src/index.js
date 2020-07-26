58af63807ce91ddc62e3ae64e0a9bbe8e149dce1
iqorange <39493393+iqorange@users.noreply.github.com>
2020-07-26 18:48:08 +0800

    koaCompose

--- Koa/betterKoa/src/index.js
+++ Koa/betterKoa/src/index.js
@@ -7,6 +7,10 @@ import path from "path"
 import helmet from 'koa-helmet'
 import statics from 'koa-static'
 import router from './routes/routes'
+import koaBody from 'koa-body'
+import jsonutil from 'koa-json'
+import cors from '@koa/cors'
+import compose from 'koa-compose'
 
 const app = new koa()
 
@@ -14,11 +18,31 @@ const app = new koa()
 // const statics = require('koa-static')
 // const router = require('./routes/routes')
 
-// 构建安全的请求头
-app.use(helmet());
-// 加载静态资源
-app.use(statics(path.join(__dirname, '../public')))
+// 更新npm包
+// ncu -u
 
+// 构建安全的请求头
+// app.use(helmet());
+// 加载静态资源
+// app.use(statics(path.join(__dirname, '../public')))
+
+// app.use(router())
+
+// 整合koa中间件，中间传递数组，使用koa-compose 集成中间件
+const middleware = compose([
+    // 整理传递消息
+    koaBody(),
+    // 静态资源
+    statics(path.join(__dirname, '../public')),
+    // 跨域请求
+    cors(),
+    // 处理JSON数据，格式化JSON数据
+    jsonutil({pretty: false, param: 'pretty'}),
+    // 请求头优化
+    helmet()
+])
+
+app.use(middleware)
 app.use(router())
 
 app.listen(3000)
