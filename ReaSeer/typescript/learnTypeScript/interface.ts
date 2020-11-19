6061d5d9aec370f033fb26b68b6363c879138dad
LiBoran <iqorange@yeah.net>
2020-11-19 08:27:29 +0800

    interface&enum

--- ReaSeer/typescript/learnTypeScript/interface.ts
+++ ReaSeer/typescript/learnTypeScript/interface.ts
@@ -7,3 +7,30 @@ let viking: Person = {
     id: 1234,
     name: 'viking'
 }
+
+interface Radio {
+    switchRadio(): void;
+}
+
+interface RadioWithBatter extends Radio {
+    checkBatteryStatus(): void;
+}
+
+interface Battery {
+    checkBatteryStatus(): void;
+}
+
+class Car implements Radio {
+    switchRadio() {
+
+    }
+}
+
+class Cellphone implements RadioWithBatter {
+    switchRadio() {
+
+    }
+    checkBatteryStatus() {
+
+    }
+}
