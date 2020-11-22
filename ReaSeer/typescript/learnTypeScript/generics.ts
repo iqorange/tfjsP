b9e5235c616a4521d96b285fc80f82bf5f403c6e
LiBoran <iqorange@yeah.net>
2020-11-22 23:06:59 +0800

    generics

--- ReaSeer/typescript/learnTypeScript/generics.ts
+++ ReaSeer/typescript/learnTypeScript/generics.ts
@@ -9,3 +9,59 @@ function swap<T, U>(tuple: [T, U]): [U, T] {
 }
 const result2 = swap(['string', 123])
 console.log(result2)
+
+function echoWithArr<T>(arg: T[]): T[] {
+    console.log(arg.length);
+    return arg;
+}
+
+const arrs = echoWithArr([1, 2, 3])
+
+interface IWithLength {
+    length: number;
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+    console.log(arg.length);
+    return arg;
+}
+
+const str = echoWithLength('str')
+const obj = echoWithLength({ length: 10, width: 10 });
+const arr2 = echoWithLength([1, 2, 3])
+
+class Queue<T> {
+    private data = [];
+    add(item: T): number {
+        return this.data.push(item);
+    }
+    remove(): T {
+        return this.data.shift();
+    }
+}
+
+const queue = new Queue<number>()
+queue.add(1)
+
+interface KeyPair<K, U> {
+    key: T;
+    value: U;
+}
+
+let kp1: KeyPair<number, string> = { key: 1, value: 'string value'}
+let kp2: KeyPair<string, number> = { key: 'string value', value: 2}
+
+let arr: number[] = [1, 2, 3]
+let arrTwo: Array<number> = [1, 2, 3]
+
+interface IPlus<T> {
+    (a: T, b: T): T
+}
+function plus(a: number, b: number): number {
+    return a + b;
+}
+function connect(a: string, b: string): string {
+    return a + b;
+}
+const a: IPlus<number> = plus
+const b: IPlus<string> = connect
