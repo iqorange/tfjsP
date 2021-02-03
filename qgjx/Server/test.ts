1131f200e840f9ec342637400055cae0b92e987a
LiBoran <iqorange@yeah.net>
2021-02-03 16:11:09 +0800

    fix

--- qgjx/Server/test.ts
+++ qgjx/Server/test.ts
@@ -1,8 +1,6 @@
-// @ts-ignore
 import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
-// @ts-ignore
 for await (const req of s) {
     req.respond({ body: "Hello World\n" });
 }
