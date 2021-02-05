eb7bed1aab33666b553435c8ae81c975cd9f8a74
LiBoran <iqorange@yeah.net>
2021-02-05 14:41:07 +0800

    indexSet

--- qgjx/Client/main.js
+++ qgjx/Client/main.js
@@ -5,8 +5,8 @@ const {
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 1366,
-        height: 768,
+        width: 1536,
+        height: 720,
         webPreferences: {
             nodeIntegration: true
         }
